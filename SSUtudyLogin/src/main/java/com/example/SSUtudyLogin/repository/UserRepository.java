97e935ac7c6c671652743b077bb9cbd720c2b0fb
Song-ki-beom <kiboem06@naver.com>
2023-05-08 17:50:44 +0900

    3

--- SSUtudyLogin/src/main/java/com/example/SSUtudyLogin/repository/UserRepository.java
+++ SSUtudyLogin/src/main/java/com/example/SSUtudyLogin/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.SSUtudyLogin.repository;
 import com.example.SSUtudyLogin.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User,Long> {
+Optional<User> findByAuthId(String AuthId);
 }
